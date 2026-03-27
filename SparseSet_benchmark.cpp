de9999e2aca5b372ebafab9eea50378ea097cb75
Marcin Kacprzak <mkacprzak@novomatic-tech.com>
2026-03-27 13:30:52 +0100

    Fixed used ranges

--- SparseSet_benchmark.cpp
+++ SparseSet_benchmark.cpp
@@ -5,14 +5,14 @@
 
 static void BM_SparseSet_contains( benchmark::State& state )
 {
-	const auto& range = benchmark::CreateDenseRange( 0, 8 << 10, 16 );
+	const auto& range = benchmark::CreateDenseRange( 0, state.range( 0 ), 16 );
 	SparseSet< std::uint16_t > set{ range.begin(), range.end() };
 
 	for( auto _ : state )
 	{
-		benchmark::DoNotOptimize( set.contains( state.range( 0 ) ) );
+		benchmark::DoNotOptimize( set.contains( state.range( 1 ) ) );
 	}
 }
 
-BENCHMARK( BM_SparseSet_contains )->Range( 8, 4096 );
+BENCHMARK( BM_SparseSet_contains )->RangeMultiplier( 64 )->Ranges( { { 0, 8 << 9 }, { 8, 8 << 8 } } );
 BENCHMARK_MAIN();
