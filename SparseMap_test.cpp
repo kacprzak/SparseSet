e0eb36d0ee83aceb6b79e1e5e2aa6159c750a319
Marcin Kacprzak <mkacprzak@novomatic-tech.com>
2026-03-29 22:56:27 +0200

    Renaming

--- SparseMap_test.cpp
+++ SparseMap_test.cpp
@@ -12,60 +12,60 @@ namespace
 
 TEST( SparseMap, ctor )
 {
-	SparseMap< std::uint8_t, float > vec;
+	SparseMap< std::uint8_t, float > map;
 
-	EXPECT_TRUE( vec.empty() );
+	EXPECT_TRUE( map.empty() );
 }
 
 TEST( SparseMap, intializer_list )
 {
-	SparseMap< std::uint16_t, float > vec{ { 1, 1.f }, { 42, 42.f }, { 16, 16.f }, { 16, 1234.f } };
+	SparseMap< std::uint16_t, float > map{ { 1, 1.f }, { 42, 42.f }, { 16, 16.f }, { 16, 1234.f } };
 
-	EXPECT_FALSE( vec.empty() );
-	EXPECT_EQ( vec.size(), 3u );
-	EXPECT_EQ( vec.at( 16 ), 16.f );
+	EXPECT_FALSE( map.empty() );
+	EXPECT_EQ( map.size(), 3u );
+	EXPECT_EQ( map.at( 16 ), 16.f );
 }
 
 TEST( SparseMap, clear )
 {
-	SparseMap< std::uint32_t, float > vec{ { 1, {} } };
+	SparseMap< std::uint32_t, float > map{ { 1, {} } };
 
-	EXPECT_FALSE( vec.empty() );
-	vec.clear();
-	EXPECT_TRUE( vec.empty() );
+	EXPECT_FALSE( map.empty() );
+	map.clear();
+	EXPECT_TRUE( map.empty() );
 }
 
 TEST( SparseMap, insert )
 {
-	SparseMap< std::uint8_t, float > vec;
+	SparseMap< std::uint8_t, float > map;
 
-	EXPECT_TRUE( vec.insert( 13, 7.f ) );
-	EXPECT_FALSE( vec.insert( 13, 13.f ) );
-	EXPECT_EQ( vec.size(), 1u );
-	EXPECT_EQ( vec.at( 13 ), 7.f );
+	EXPECT_TRUE( map.insert( 13, 7.f ) );
+	EXPECT_FALSE( map.insert( 13, 13.f ) );
+	EXPECT_EQ( map.size(), 1u );
+	EXPECT_EQ( map.at( 13 ), 7.f );
 
-	EXPECT_ANY_THROW( vec.insert( 255, {} ) );
+	EXPECT_ANY_THROW( map.insert( 255, {} ) );
 }
 
 TEST( SparseMap, erase )
 {
-	SparseMap< std::uint16_t, float > vec{ { 0, 0.f }, { 1, 1.f }, { 2, 2.f } };
+	SparseMap< std::uint16_t, float > map{ { 0, 0.f }, { 1, 1.f }, { 2, 2.f } };
 
-	EXPECT_TRUE( vec.erase( 1 ) );
-	EXPECT_EQ( vec.size(), 2u );
-	EXPECT_FALSE( vec.erase( 1 ) );
-	EXPECT_EQ( vec.size(), 2u );
+	EXPECT_TRUE( map.erase( 1 ) );
+	EXPECT_EQ( map.size(), 2u );
+	EXPECT_FALSE( map.erase( 1 ) );
+	EXPECT_EQ( map.size(), 2u );
 
-	EXPECT_TRUE( vec.contains( 0 ) );
-	EXPECT_EQ( vec.at( 2 ), 2.f );
+	EXPECT_TRUE( map.contains( 0 ) );
+	EXPECT_EQ( map.at( 2 ), 2.f );
 }
 
 TEST( SparseMap, iterator )
 {
-	SparseMap< std::uint16_t, float > vec{ { 0, 0.f }, { 1, 1.f }, { 2, 2.f } };
+	SparseMap< std::uint16_t, float > map{ { 0, 0.f }, { 1, 1.f }, { 2, 2.f } };
 
 	int sum = 0;
-	for( const auto& [ k, v ] : vec )
+	for( const auto& [ k, v ] : map )
 		sum += k;
 
 	EXPECT_EQ( sum, 3 );
@@ -73,10 +73,10 @@ TEST( SparseMap, iterator )
 
 TEST( SparseMap, const_iterator )
 {
-	const SparseMap< std::uint16_t, float > vec{ { 0, 0.f }, { 1, 1.f }, { 2, 2.f } };
+	const SparseMap< std::uint16_t, float > map{ { 0, 0.f }, { 1, 1.f }, { 2, 2.f } };
 
 	int sum = 0;
-	for( const auto& [ k, v ] : vec )
+	for( const auto& [ k, v ] : map )
 		sum += k;
 
 	EXPECT_EQ( sum, 3 );
@@ -84,70 +84,70 @@ TEST( SparseMap, const_iterator )
 
 TEST( SparseMap, sort )
 {
-	SparseMap< std::uint8_t, float > vec{ { 3, {} }, { 1, {} }, { 0, {} }, { 2, {} }, { 5, {} } };
+	SparseMap< std::uint8_t, float > map{ { 3, {} }, { 1, {} }, { 0, {} }, { 2, {} }, { 5, {} } };
 
 	const std::vector< std::uint8_t > presort{ 3, 1, 0, 2, 5 };
 	const std::vector< std::uint8_t > postsort{ 0, 1, 2, 3, 5 };
 
-	EXPECT_FALSE( vec.contains( 4 ) );
+	EXPECT_FALSE( map.contains( 4 ) );
 
 	{
-		const auto keys_view = std::ranges::views::keys( vec );
+		const auto keys_view = std::ranges::views::keys( map );
 		EXPECT_TRUE( std::ranges::equal( keys_view, presort ) );
 	}
 
-	vec.sort();
+	map.sort();
 
-	EXPECT_FALSE( vec.contains( 4 ) );
+	EXPECT_FALSE( map.contains( 4 ) );
 
 	{
-		const auto keys_view = std::ranges::views::keys( vec );
+		const auto keys_view = std::ranges::views::keys( map );
 		EXPECT_TRUE( std::ranges::equal( keys_view, postsort ) );
 	}
 
 	// Checks if sparse vector is updated.
-	vec.insert( 3, {} );
+	map.insert( 3, {} );
 
 	{
-		const auto keys_view = std::ranges::views::keys( vec );
+		const auto keys_view = std::ranges::views::keys( map );
 		EXPECT_TRUE( std::ranges::equal( keys_view, postsort ) );
 	}
 }
 
 TEST( SparseMap, at )
 {
-	SparseMap< std::uint8_t, float > vec;
+	SparseMap< std::uint8_t, float > map;
 
-	vec.insert( 42, 13.f );
-	vec.at( 42 ) = 21.f;
-	EXPECT_EQ( vec.at( 42 ), 21.f );
+	map.insert( 42, 13.f );
+	map.at( 42 ) = 21.f;
+	EXPECT_EQ( map.at( 42 ), 21.f );
 }
 
 TEST( SparseMap, find_slot )
 {
-	SparseMap< std::uint8_t, float > vec;
+	SparseMap< std::uint8_t, float > map;
 
 	for( int i = 0; i < std::numeric_limits< std::uint8_t >::max(); ++i )
-		vec.insert( i, i );
+		map.insert( i, i );
 
 	// Erase second element
-	EXPECT_EQ( vec.at( 1 ), 1.f );
-	EXPECT_TRUE( vec.erase( 1 ) );
+	EXPECT_EQ( map.at( 1 ), 1.f );
+	EXPECT_TRUE( map.erase( 1 ) );
 
 	// Should reuse index 1
-	EXPECT_EQ( vec.find_slot(), 1 );
-	vec.insert( vec.find_slot(), 4.f );
-	EXPECT_EQ( vec.at( 1 ), 4.f );
+	EXPECT_EQ( map.find_slot(), 1 );
+	map.insert( map.find_slot(), 4.f );
+	EXPECT_EQ( map.at( 1 ), 4.f );
 
 	// Erase first element
-	EXPECT_TRUE( vec.erase( 0 ) );
-	EXPECT_EQ( vec.at( 1 ), 4.f );
+	EXPECT_TRUE( map.erase( 0 ) );
+	EXPECT_EQ( map.at( 1 ), 4.f );
 
 	// Should reuse index 0
-	EXPECT_EQ( vec.find_slot(), 0 );
-	vec.insert( vec.find_slot(), 2.f );
-	EXPECT_EQ( vec.at( 0 ), 2.f );
-	EXPECT_EQ( vec.at( 1 ), 4.f );
+	EXPECT_EQ( map.find_slot(), 0 );
+	map.insert( map.find_slot(), 2.f );
+	EXPECT_EQ( map.at( 0 ), 2.f );
+	EXPECT_EQ( map.at( 1 ), 4.f );
 }
 
 } // namespace
--- SparseSet_test.cpp
+++ SparseSet_test.cpp
@@ -9,60 +9,60 @@ namespace
 
 TEST( SparseSet, ctor )
 {
-	SparseSet< std::uint8_t > vec;
+	SparseSet< std::uint8_t > set;
 
-	EXPECT_TRUE( vec.empty() );
+	EXPECT_TRUE( set.empty() );
 }
 
 TEST( SparseSet, intializer_list )
 {
-	SparseSet< std::uint16_t > vec{ 1, 6, 32, 5, 42, 16, 16, 16, 16, 16 };
+	SparseSet< std::uint16_t > set{ 1, 6, 32, 5, 42, 16, 16, 16, 16, 16 };
 
-	EXPECT_FALSE( vec.empty() );
-	EXPECT_EQ( vec.size(), 6u );
-	EXPECT_TRUE( vec.contains( 42 ) );
+	EXPECT_FALSE( set.empty() );
+	EXPECT_EQ( set.size(), 6u );
+	EXPECT_TRUE( set.contains( 42 ) );
 }
 
 TEST( SparseSet, clear )
 {
-	SparseSet< std::uint32_t > vec{ 1 };
+	SparseSet< std::uint32_t > set{ 1 };
 
-	EXPECT_FALSE( vec.empty() );
-	vec.clear();
-	EXPECT_TRUE( vec.empty() );
+	EXPECT_FALSE( set.empty() );
+	set.clear();
+	EXPECT_TRUE( set.empty() );
 }
 
 TEST( SparseSet, insert )
 {
-	SparseSet< std::uint8_t > vec;
+	SparseSet< std::uint8_t > set;
 
-	EXPECT_TRUE( vec.insert( 13 ) );
-	EXPECT_FALSE( vec.insert( 13 ) );
-	EXPECT_EQ( vec.size(), 1u );
-	EXPECT_TRUE( vec.contains( 13 ) );
+	EXPECT_TRUE( set.insert( 13 ) );
+	EXPECT_FALSE( set.insert( 13 ) );
+	EXPECT_EQ( set.size(), 1u );
+	EXPECT_TRUE( set.contains( 13 ) );
 
-	EXPECT_ANY_THROW( vec.insert( 255 ) );
+	EXPECT_ANY_THROW( set.insert( 255 ) );
 }
 
 TEST( SparseSet, erase )
 {
-	SparseSet< std::uint16_t > vec{ 0, 1, 2 };
+	SparseSet< std::uint16_t > set{ 0, 1, 2 };
 
-	EXPECT_TRUE( vec.erase( 1 ) );
-	EXPECT_EQ( vec.size(), 2u );
-	EXPECT_FALSE( vec.erase( 1 ) );
-	EXPECT_EQ( vec.size(), 2u );
+	EXPECT_TRUE( set.erase( 1 ) );
+	EXPECT_EQ( set.size(), 2u );
+	EXPECT_FALSE( set.erase( 1 ) );
+	EXPECT_EQ( set.size(), 2u );
 
-	EXPECT_TRUE( vec.contains( 0 ) );
-	EXPECT_TRUE( vec.contains( 2 ) );
+	EXPECT_TRUE( set.contains( 0 ) );
+	EXPECT_TRUE( set.contains( 2 ) );
 }
 
 TEST( SparseSet, iterator )
 {
-	SparseSet< std::uint64_t > vec{ 0, 1, 2, 3 };
+	SparseSet< std::uint64_t > set{ 0, 1, 2, 3 };
 
 	int sum = 0;
-	for( const auto& value : vec )
+	for( const auto& value : set )
 		sum += value;
 
 	EXPECT_EQ( sum, 6 );
@@ -70,10 +70,10 @@ TEST( SparseSet, iterator )
 
 TEST( SparseSet, const_iterator )
 {
-	const SparseSet< std::uint8_t > vec{ 0, 1, 2, 3 };
+	const SparseSet< std::uint8_t > set{ 0, 1, 2, 3 };
 
 	int sum = 0;
-	for( const auto& value : vec )
+	for( const auto& value : set )
 		sum += value;
 
 	EXPECT_EQ( sum, 6 );
@@ -81,22 +81,22 @@ TEST( SparseSet, const_iterator )
 
 TEST( SparseSet, sort )
 {
-	SparseSet< std::uint8_t > vec{ 3, 1, 0, 2, 5 };
+	SparseSet< std::uint8_t > set{ 3, 1, 0, 2, 5 };
 
 	const std::vector< std::uint8_t > presort{ 3, 1, 0, 2, 5 };
 	const std::vector< std::uint8_t > postsort{ 0, 1, 2, 3, 5 };
 
-	EXPECT_FALSE( vec.contains( 4 ) );
-	EXPECT_TRUE( std::equal( vec.begin(), vec.end(), presort.begin(), presort.end() ) );
+	EXPECT_FALSE( set.contains( 4 ) );
+	EXPECT_TRUE( std::equal( set.begin(), set.end(), presort.begin(), presort.end() ) );
 
-	vec.sort();
+	set.sort();
 
-	EXPECT_FALSE( vec.contains( 4 ) );
-	EXPECT_TRUE( std::equal( vec.begin(), vec.end(), postsort.begin(), postsort.end() ) );
+	EXPECT_FALSE( set.contains( 4 ) );
+	EXPECT_TRUE( std::equal( set.begin(), set.end(), postsort.begin(), postsort.end() ) );
 
 	// Checks if sparse vector is updated.
-	vec.insert( 3 );
-	EXPECT_TRUE( std::equal( vec.begin(), vec.end(), postsort.begin(), postsort.end() ) );
+	set.insert( 3 );
+	EXPECT_TRUE( std::equal( set.begin(), set.end(), postsort.begin(), postsort.end() ) );
 }
 
 } // namespace
